56822351d027f3302838e8ec9e2ce8c230a4c0f6
Troels Thomsen <troels@thomsen.io>
2013-06-18 15:28:34 +0200

    Implement execution of commands

--- app.rb
+++ app.rb
@@ -1,1 +1,43 @@
+require 'base64'
+require 'open3'
 require 'sinatra'
+
+require './lib/event_response'
+
+get '/commands/*', provides: 'text/event-stream' do
+  command_class = nil
+
+  not_found if command_class.nil?
+
+  command = command_class.new(params)
+
+  stream(:keep_open) do |out|
+    response = EventResponse.new(out)
+
+    stdin, stdout, stderr = Open3.popen3(command.to_s)
+
+    reads = [stdout, stderr]
+
+    mapping = {
+      stdout => 'out',
+      stderr => 'err'
+    }
+
+    while reads.length > 0
+      (inputs, _, _) = IO.select(reads)
+
+      inputs.each do |input|
+        if input.eof?
+          reads.delete(input)
+          break
+        end
+
+        bytes = input.read_nonblock(1024)
+
+        response.write mapping.fetch(input), Base64.strict_encode64(bytes)
+      end
+    end
+
+    response.close
+  end
+end
