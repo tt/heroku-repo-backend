e02d48f78c1e6ff7c6b73fa671f044d3c114d10c
Troels Thomsen <troels@thomsen.io>
2013-06-22 00:29:43 +0200

    Verify that process is spawned in temporary directory

--- spec/command_runner_spec.rb
+++ spec/command_runner_spec.rb
@@ -16,6 +16,14 @@ describe CommandRunner do
       subject.execute('echo foo 1>&2', nil, err)
     end
 
+    it 'spawns in work directory' do
+      Dir.stub(:mktmpdir) { '/tmp' }
+      FileUtils.stub(:rm_r)
+      IO.stub(:join)
+      Open3.should_receive(:popen3).with(nil, :chdir => '/tmp')
+      subject.execute(nil, nil, nil)
+    end
+
     it 'removes work directory' do
       Dir.stub(:mktmpdir) { '/tmp' }
       FileUtils.should_receive(:rm_r).with('/tmp')
