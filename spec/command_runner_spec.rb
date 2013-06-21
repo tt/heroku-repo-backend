4395e53d6e46998d69898103a1bbbacb55531d69
Troels Thomsen <troels@thomsen.io>
2013-06-21 19:37:09 +0200

    Verify that errors are written

--- spec/command_runner_spec.rb
+++ spec/command_runner_spec.rb
@@ -11,5 +11,13 @@ describe CommandRunner do
       out.should_receive(:write).with("foo\n")
       subject.execute(command, out, nil)
     end
+
+    it 'writes errors' do
+      command = stub
+      command.stub(:to_s) {|dir| 'echo foo 1>&2' }
+      err = mock
+      err.should_receive(:write).with("foo\n")
+      subject.execute(command, nil, err)
+    end
   end
 end
